e6095ab091c017f89fbd3f1cd840a761ac18ac0d
TamNguyen8021 <tamntt8021@gmail.com>
2023-06-17 13:06:39 +0700

    style: Change single-quotes to double-quotes

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,6 +1,6 @@
-import 'css/Book.css';
-import BookCover from './BookCover';
-import HoverButton from 'components/HoverButton';
+import "css/Book.css";
+import BookCover from "./BookCover";
+import HoverButton from "components/HoverButton";
 import { useEffect, useState } from "react";
 import { editBook, getBooks } from "BooksAPI";
 
@@ -23,21 +23,21 @@ const Book = (props) => {
 	};
 
 	return (
-		<div className='book'>
-			<div className='book-top'>
+		<div className="book">
+			<div className="book-top">
 				{props?.cover && <BookCover cover={props.cover} />}
 				<HoverButton
 					shelf={shelf}
 					changeBookShelf={changeBookShelf}
 				/>
 			</div>
-			{props?.title && <div className='book-title'>{props.title}</div>}
-			{props?.subtitle && <div className='book-subtitle'>{props.subtitle}</div>}
+			{props?.title && <div className="book-title">{props.title}</div>}
+			{props?.subtitle && <div className="book-subtitle">{props.subtitle}</div>}
 			{props?.authors &&
 				props.authors.map((author, index) => (
 					<div
 						key={index + Math.random() * 100}
-						className='book-authors'>
+						className="book-authors">
 						{author}
 					</div>
 				))}
