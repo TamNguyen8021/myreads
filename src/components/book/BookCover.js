447edc1c22a8d28614965757b6f5b8263a494666
TamNguyen8021 <tamntt8021@gmail.com>
2023-06-17 10:17:29 +0700

    fix: Fix book's cover not shown

--- src/components/book/BookCover.js
+++ src/components/book/BookCover.js
@@ -1,13 +1,17 @@
+import "css/BookCover.css";
+
 /**
  * @description Represents a book's cover image
  */
 const BookCover = (props) => {
 	return (
-		<div
-			className='book-cover'
-			style={{
-				backgroundImage: `${props.cover}`,
-			}}></div>
+		<div>
+			<img
+				className="book-cover"
+				src={props.cover}
+				alt="Book's thumbnail"
+			/>
+		</div>
 	);
 };
 
