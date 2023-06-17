2334e882deeac65b8e0cfd609a4128a8d677d7cc
TamNguyen8021 <tamntt8021@gmail.com>
2023-06-17 20:20:37 +0700

    style: Change single-quotes to double-quotes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'css/index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "css/index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SearchPage from "components/pages/SearchPage";
 
