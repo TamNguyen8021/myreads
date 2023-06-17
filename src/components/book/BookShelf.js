6e6296a33b92be789b7ffe2c8cde64190130fd3c
TamNguyen8021 <tamntt8021@gmail.com>
2023-06-17 19:15:43 +0700

    style: Change single-quotes to double-quotes

--- src/components/book/BookShelf.js
+++ src/components/book/BookShelf.js
@@ -1,11 +1,11 @@
-import 'css/BookShelf.css';
+import "css/BookShelf.css";
 
 /**
  * @description Represents a book shelf
  */
 const BookShelf = (props) => {
 	return (
-		<div className='bookshelf'>
+		<div className="bookshelf">
 			<h2 className="bookshelf-title">{props.name}</h2>
 			{props?.children?.length ? (
 				<div className="bookshelf-books">
