0ae2aee138068bd8752b60bf0362fba0fb859d97
TamNguyen8021 <tamntt8021@gmail.com>
2023-06-17 10:20:22 +0700

    fix: Change category's id to map with data from API

--- src/components/CategoriesContext.js
+++ src/components/CategoriesContext.js
@@ -1,9 +1,9 @@
 const { createContext } = require('react');
 
 const CategoriesContext = createContext([
-	{ id: 1, name: 'Currently Reading' },
-	{ id: 2, name: 'Want to Read' },
-	{ id: 3, name: 'Read' },
+	{ id: 'currentlyReading', name: 'Currently Reading' },
+	{ id: 'wantToRead', name: 'Want to Read' },
+	{ id: 'read', name: 'Read' },
 ]);
 
 export default CategoriesContext;
