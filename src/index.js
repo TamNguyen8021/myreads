2f5df724c43dcc24e2d28bf4a5c91fd7d8ecb70e
TamNguyen8021 <tamntt8021@gmail.com>
2023-06-18 09:43:05 +0700

    refactor: Remove unused files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "css/index.css";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import SearchPage from "components/pages/SearchPage";
 
@@ -26,8 +25,3 @@ root.render(
 		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
